239a088437611be2272a2a60ca47d804450d8452
sikkkkkw <gksmsla92@naver.com>
2024-01-23 23:16:39 +0900

    20240123

--- dark.js
+++ dark.js
@@ -1,7 +1,14 @@
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  document.documentElement.classList.add("dark");
-}
-
-document.getElementById("toggleTheme").addEventListener("click", () => {
+document.getElementById("darkModeToggle").addEventListener("click", () => {
   document.documentElement.classList.toggle("dark");
+  const darkModeIcon = document.getElementById('darkModeIcon');
+  const lightModeIcon = document.getElementById('lightModeIcon');
+
+  if (document.documentElement.classList.contains("dark")) {
+    darkModeIcon.style.display = 'none';
+    lightModeIcon.style.display = 'inline-block';
+
+  } else {
+    darkModeIcon.style.display = 'inline-block';
+    lightModeIcon.style.display = 'none ';
+  }
 });
